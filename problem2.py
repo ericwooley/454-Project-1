1d2ac267d333d2a5f75c216d381682f3b37ff145
Eric Wooley <5wooley4@gmail.com>
2013-10-13 15:47:59 -0700

    Inital commit

--- problem2.py
+++ problem2.py
@@ -3,24 +3,18 @@ from lib.Automata import *
 from pprint import pprint
 
 
-states = {}
+delta = {}
 
 x = int(input("Enter k: "))
 # this is for a test
 for i in range(0, x):
-	print "========================= state: ", i, "===================================="
-	states[i] = {}
+	delta[str(i)] = {}
 	for j in range(0,10):
-		states[i][j] = set(str(int(str(i)+str(j)) % x));
-		print "Input:", j, "--->", int(str(i)+str(j)) % x
-		print
-delta = {'A' : { '0' : set(['B']), '1': set(['B','C'])}, 
-	'B': {'0': set(['C']), '1':set(['D'])}, 
-	'C': {'0':set(['A', 'B']), '1': set(['D'])}, 
-	'D': {'0': set(['D']), '1': set(['D'])}
- } 
-alphabet = set(['0', '1', '2', '3', '4', '5', '6', '7', '8' ,'9']);
-nfa = LookupNFA(alphabet, 'A', states, 'D')
+		delta[str(i)][str(j)] = set([str(int(str(i)+str(j)) % x), str(i)+'`']);
+for i in range(0, x):
+	delta[str(i)+'`'] = {}
+	for j in range(0,10):
+		delta[str(i)+'`'][str(j)] = set(str(int(str(i)+str(j)) % x));
 pprint(delta)
 
 
