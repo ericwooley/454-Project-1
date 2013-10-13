ff31b2d78535811930781af00ae076ed365caa92
Eric Wooley <5wooley4@gmail.com>
2013-10-13 16:01:09 -0700

    removed extra shit

--- problem2.py
+++ problem2.py
@@ -1,5 +1,3 @@
-# Import DFA from Eppstein
-from lib.Automata import *
 from pprint import pprint
 
 
@@ -23,14 +21,3 @@ for i in range(0, x):
 delta['0']['Accepting'] = True
 delta['0`']['Accepting'] = True
 pprint(delta)
-
-
-
-#this prints stuff. idk if it works or not or what.
-# alphabet = set([0, 1, 2, 3, 4, 5, 6, 7, 8,9]);
-# delta = { 'A' : { '0' : set(['B']), '1': set(['B','C'])}, 
-#  'B': {'0': set(['C']), '1':set(['D'])}, 
-#  'C': {'0':set(['A', 'B']), '1': set(['D'])}, 
-#  'D': {'0': set(['D']), '1': set(['D'])}} 
-# N = LookupNFA("ABCD", delta, 'A',['C'])
-# pprint(delta)
