6f6c23aa1b6153cf13ebe87f0e6c0740833343df
Eric Wooley <5wooley4@gmail.com>
2013-10-13 15:58:26 -0700

    updated question to also print accepting states.

--- problem2.py
+++ problem2.py
@@ -9,12 +9,19 @@ x = int(input("Enter k: "))
 # this is for a test
 for i in range(0, x):
 	delta[str(i)] = {}
+	delta[str(i)]['transitions'] = {}
+	delta[str(i)]['Accepting'] = False
 	for j in range(0,10):
-		delta[str(i)][str(j)] = set([str(int(str(i)+str(j)) % x), str(i)+'`']);
+		delta[str(i)]['transitions'][str(j)]=set([str(int(str(i)+str(j)) % x), str(i)+'`'])
 for i in range(0, x):
 	delta[str(i)+'`'] = {}
+	delta[str(i)+'`']['transitions'] = {}
+	delta[str(i)+'`']['Accepting'] = False
 	for j in range(0,10):
-		delta[str(i)+'`'][str(j)] = set(str(int(str(i)+str(j)) % x));
+		delta[str(i)+'`']['transitions'][str(j)] = set(str(int(str(i)+str(j)) % x))
+
+delta['0']['Accepting'] = True
+delta['0`']['Accepting'] = True
 pprint(delta)
 
 
