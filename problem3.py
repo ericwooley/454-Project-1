23ce676ca2f3625201907ff85734159e2d9bf85e
Eric Wooley <5wooley4@gmail.com>
2013-10-13 17:12:06 -0700

    update to p3 still doesn't work

--- problem3.py
+++ problem3.py
@@ -8,13 +8,14 @@ delta = {}
 x = int(input("Enter k: "))
 # this is for a test
 for i in range(0, x):
-	delta[i] = {}
 	for j in range(0,10):
-		delta[i][j]=set([int(str(i)+str(j)) % x, x+i])
-for i in range(0, x):
-	delta[x+i] = {}
-	for j in range(0,10):
-		delta[x+i][j] = set([str(int(str(i)+str(j)) % x)])
+		# delta['(' + str(i)+', '+str(j)+')' ] = set([int(str(i)+str(j)) % x, x+i])
+		delta[(str(i), str(j))] = (str(int(str(i)+str(j)) % x), str(x+i))
+# for i in range(0, x):
+# 	delta[x+i] = {}
+# 	for j in range(0,10):
+# 		delta[x+i][j] = set([str(int(str(i)+str(j)) % x)])
 pprint (delta)
-N = LookupNFA("0123456789", set([0]), delta, set([0, x]) )
+
+N = LookupNFA("0123456789", set(['0']), delta, set([0, x]) )
 N.pprint()
