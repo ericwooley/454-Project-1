6c8cea7681261ed9c22ec681c197ac4d61d89c85
Eric Wooley <5wooley4@gmail.com>
2013-10-13 18:16:35 -0700

    update to p3

--- problem3.py
+++ problem3.py
@@ -10,12 +10,11 @@ x = int(input("Enter k: "))
 for i in range(0, x):
 	for j in range(0,10):
 		# delta['(' + str(i)+', '+str(j)+')' ] = set([int(str(i)+str(j)) % x, x+i])
-		delta[(str(i), str(j))] = (str(int(str(i)+str(j)) % x), str(x+i))
-# for i in range(0, x):
-# 	delta[x+i] = {}
-# 	for j in range(0,10):
-# 		delta[x+i][j] = set([str(int(str(i)+str(j)) % x)])
-pprint (delta)
+		delta[(str(i), str(j))] = (str(int(str(i)+str(j)) % x), str(i)+'`')
+for i in range(0, x):
+	for j in range(0,10):
+		delta[(str(i)+'`', str(j))] = (str(int(str(i)+str(j)) % x))
+# pprint (delta)
 
-N = LookupNFA(['0','1','2','3','4','5','6','7','8','9'], set(['0']), delta, set([0, x]) )
-N.pprint()
+N = LookupNFA(['0','1','2','3','4','5','6','7','8','9'], ['0'], delta, ['0', str(x)] )
+N.asDFA().minimize().pprint()
