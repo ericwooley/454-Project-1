aa29be50724d3085111b147d889224ddade021e7
Eric Wooley <5wooley4@gmail.com>
2013-10-13 17:40:34 -0700

    fixed problem 2

--- problem2.py
+++ problem2.py
@@ -16,7 +16,7 @@ for i in range(0, x):
 	delta[str(i)+'`']['transitions'] = {}
 	delta[str(i)+'`']['Accepting'] = False
 	for j in range(0,10):
-		delta[str(i)+'`']['transitions'][str(j)] = set(str(int(str(i)+str(j)) % x))
+		delta[str(i)+'`']['transitions'][str(j)] = set([str(int(str(i)+str(j)) % x)+'`'])
 
 delta['0']['Accepting'] = True
 delta['0`']['Accepting'] = True
